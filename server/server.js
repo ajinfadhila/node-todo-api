4dadb7fef4cc087ce6f40082797ff7131b230510
ajinfadhila <ajinfadhila@gmail.com>
2018-11-18 17:06:04 +0700

    coba

--- server/server.js
+++ server/server.js
@@ -1,10 +1,6 @@
-const express = require("express");
-const bodyParser = require("body-parser")
-const {ObjectID} = require("mongodb")
-// console.log(bodyParser);
-const{mongoose}= require("./db/mongoose.js");
-const{Todo} = require("./models/todo.js");
-const{User} = require("./models/user.js");
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,5 +10,3 @@ var server = http.Server(app);
 server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
-
-module.exports = {app}
