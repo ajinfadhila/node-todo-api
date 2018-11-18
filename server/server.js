de5950e9ec2f87a2a063cda1650eb67aeb76f970
ajinfadhila <ajinfadhila@gmail.com>
2018-11-18 16:27:50 +0700

    up package json

--- server/server.js
+++ server/server.js
@@ -12,45 +12,6 @@ var port = process.env.PORT || 3000;
 var http = require('http');
 var server = http.Server(app);
 
-app.use(bodyParser.json());
-app.post('/todos',(req,res)=>{
-  var todo = new Todo({
-    text: req.body.text
-  });
-  todo.save().then((doc)=>{
-    res.send(doc);
-  },(e)=>{
-    res.status(400).send(e);
-  });
-});
-
-app.get('/todos',(req,res)=>{
-  Todo.find().then((todos)=>{
-    res.send({todos});
-  },(e)=>{
-    res.status(400).send(e);
-  })
-});
-
-
-app.get('/todos/:id',(req,res)=>{
-  var id = req.params.id
-
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
-
-  Todo.findById(id).then((todo)=>{
-    if (!todo) {
-      return res.status(400).send();
-    }
-    // res.send(req.params);
-    console.log(req.params.id);
-    res.send({todo});
-  }).catch((e)=>{
-    res.status(400).send();
-  })
-})
 server.listen(port,()=>{
   console.log(`server run port ${port}`);
 });
