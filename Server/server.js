411c479a81729991279433a4f7a273c386fd52f5
ajinfadhila <ajinfadhila@gmail.com>
2018-11-18 17:51:53 +0700

    coba

--- Server/server.js
+++ Server/server.js
@@ -3,14 +3,6 @@ const hbs = require('hbs');
 const fs = require('fs');
 
 
-const express = require("express");
-const bodyParser = require("body-parser")
-const {ObjectID} = require("mongodb")
-// console.log(bodyParser);
-const{mongoose}= require("./db/mongoose.js");
-const{Todo} = require("./models/todo.js");
-const{User} = require("./models/user.js");
-
 const port = process.env.PORT || 3000;
 var app = express();
 var http = require('http');
