9a5560855c96b9134c0a0929b0a615093d7cfdf5
ajinfadhila <ajinfadhila@gmail.com>
2018-11-18 17:54:47 +0700

    coba

--- Server/server.js
+++ Server/server.js
@@ -14,6 +14,18 @@ var app = express();
 var http = require('http');
 var server = http.Server(app);
 
+app.use(bodyParser.json());
+app.post('/todos',(req,res)=>{
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(e)=>{
+    res.status(400).send(e);
+  });
+});
+
 server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
