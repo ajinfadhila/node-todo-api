5e74c9d275fe97de065c2a876ffa90a9b0a98cd7
ajinfadhila <ajinfadhila@gmail.com>
2018-11-25 21:04:01 +0700

    update terbaru

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.post('/todos',(req,res)=>{
 app.get('/todos',(req,res)=>{
   Todo.find().then((todos)=>{
     // console.log(object.keys(todos));
-    res.send({todos});
+    res.send(todos);
   },(e)=>{
     res.status(400).send(e);
   })
